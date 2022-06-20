45fb5ee4a4063428ba968d3c668ba07ad8a9a60f
Evanilsonpg <99387388+Evanilsonpg@users.noreply.github.com>
2022-06-19 21:38:38 -0300

    readme finalizado

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const passwordConfirmation = document.getElementById("password-confirmation");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  checkInputs();
+  checkInputs(); 
 });
 
 function checkInputs() {
